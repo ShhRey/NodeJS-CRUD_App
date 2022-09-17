f08d460b26a698b6b91d190d42b1d0a5c83b9591
Shrey Trivedi <41701281+ShyGuyST@users.noreply.github.com>
2022-09-17 17:59:46 +0530

    Referral Code and Count Updates
    Users can add referrals under them and referral count will update accordingly

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const SpeakEasy = require('speakeasy');
-const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
@@ -42,8 +40,8 @@ router.post('/register', async(req, res) => {
                 var secret_key = SpeakEasy.generateSecret({ length: 20 }).base32;
                 var referral_code = SpeakEasy.generateSecret({ length: 10}).base32;
                 const user = new User({ user_name, email_add, password, conf_password, secret_key, referral_code });
-                const newUser = await user.save(); 
-                res.status(200).json(newUser);
+                await user.save(); 
+                res.status(200).json(`User: ${user_name} Successfully Registered`);
             }
         }
         catch(err) {
@@ -52,7 +50,41 @@ router.post('/register', async(req, res) => {
 })
 
 
+// Register User via Referral Links / Code
+router.post(`/register/:referral_code`, async(req, res) => {
+    try {
+        const sponsor = await User.findOne({'referral_code':req.params.referral_code});
+        const { user_name, email_add, password, conf_password } = req.body;
+        if (!user_name || !email_add || !password || !conf_password) {
+            return res.status(400).json({ erorr: "All Fields Required" });
+        }
+        const existingUser = await User.findOne({ user_name });
+        if (existingUser) {
+            return res.status(400).json({ error: "Username already taken !" });
+        }
+        const existingEmail = await User.findOne({ email_add });
+        if (existingEmail) {
+            return res.status(400).json({ error: "Email already taken !" });
+        }
+        if (password != conf_password){
+            return res.status(400).json({error: "The Password did not match !"});  
+        }       
+        else{ 
+            var secret_key = SpeakEasy.generateSecret({ length: 20 }).base32;
+            var referral_code = SpeakEasy.generateSecret({ length: 10}).base32;
+            var referred_by = sponsor._id
 
+            sponsor.referral_count = sponsor.referral_count + 1
+            await sponsor.save();
+            const user = new User({ user_name, email_add, password, secret_key, referral_code, referred_by });
+            await user.save(); 
+            res.status(200).json(`User: ${user_name} Successfully Registered with ${sponsor.user_name}'s referral`);
+        }
+    } 
+    catch(err) {
+        res.send('Error: '+err)
+    }
+})
 
 
 // Query Particular User Details based on Object ID
@@ -70,29 +102,26 @@ router.get('/user/:id', async(req, res) => {
 // Route for Loging into Web Application
 router.post('/login', async(req, res) => {
     try {
-        const { email_add, password, referral_code } = req.body;
-
-        // Verify User Referral
-        
+        const { email_add, password } = req.body;
         // Verify User Details present in DB
         let user = await User.findOne({ email_add });
         if (!user) {
-        return res.status(400).json({ msg: 'Email or password incorrect' });
+            return res.status(400).json({ error: "Incorrect Email !" });
         }
-
         // Check if the encrypted password matches to Users Input
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-        return res.status(400).json({ msg: 'Email or password incorrect' });
+            return res.status(400).json({error: "Incorrect Password !"});
         }
         else {
-            res.status(200).send(`User: ${user.user_name} Successfully Logged In with Referral Code : ${user.referral_code}`)
+            res.status(200).send(`User: ${user.user_name} Successfully Logged In`)
         }
     }
     catch(err) {
         res.status(500).send("Error: "+err);
     }
 });
+
 
 
 // Route for Token Verification for 2FA
